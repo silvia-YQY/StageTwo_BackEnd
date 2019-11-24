2ef91da77d2e1fb51ca3d7f38844a1bbf3e2fec1
silvia-YQY <545067444@qq.com>
2019-11-24 13:46:08 +0800

    添加任务标记/删除/修改功能

--- node-todo/index.js
+++ node-todo/index.js
@@ -26,11 +26,50 @@ module.exports.showAll = async () => {
                     return {
                         name: `${task.done ? '[x]' : '[_]'} ${index + 1} - ${task.title}`, value: index.toString()
                     }
-                })]
+                }), { name: '+ 创建任务', value: '-2' }]
             }
         ])
         .then(answers => {
-            // list[answers.index].done = !list[answers.index].done
-            console.log(answers);
+            const index = parseInt(answers.index)
+            if (index >= 0) {
+                inquirer.prompt({
+                    type: 'list', name: 'action',
+                    message: '请选择操作',
+                    choices: [
+                        { name: '退出', value: 'quit' },
+                        { name: '已完成', value: 'markAsDone' },
+                        { name: '未完成', value: 'markAsUndone' },
+                        { name: '改标题', value: 'updateTitle' },
+                        { name: '删除', value: 'remove' }
+                    ]
+                }).then(answers2 => {
+                    switch (answers2.action) {
+                        case 'markAsDone':
+                            list[index].done = true
+                            db.write(list)
+                            break;
+                        case 'markAsUndon':
+                            list[index].done = false
+                            db.write(list)
+                            break;
+                        case 'updateTitle':
+                            inquirer.prompt({
+                                type: 'input',
+                                name: 'title',
+                                message: '新的标题为？',
+                                default: list[index].title
+                            }).then(answer => {
+                                list[index].title = answer.title
+                                db.write(list)
+                            });
+                            break;
+                        case 'remove':
+                            list.splice(index, 1)
+                            break;
+                    }
+                })
+            } else if (index === '-2') {
+                console.log(answers);
+            }
         });
 }
