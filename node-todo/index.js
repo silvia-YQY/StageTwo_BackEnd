4ec318e2ec750886a71988d95ecca00c3b4c3a61
silvia-YQY <545067444@qq.com>
2019-11-24 12:52:07 +0800

    添加inquirer库，一个命令行工具交互库

--- node-todo/index.js
+++ node-todo/index.js
@@ -1,4 +1,5 @@
 const db = require('./db.js')
+const inquirer = require('inquirer')
 
 module.exports.add = async (title) => {
     // 读取之前的任务
@@ -18,5 +19,35 @@ module.exports.showAll = async () => {
     list.forEach((task, index) => {
         console.log(`${task.done ? '[x]' : '[_]'} ${index + 1} - ${task.title}`);
     });
-
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'theme',
+                message: 'What do you want to do?',
+                choices: [
+                    'Order a pizza',
+                    'Make a reservation',
+                    new inquirer.Separator(),
+                    'Ask for opening hours',
+                    {
+                        name: 'Contact support',
+                        disabled: 'Unavailable at this time'
+                    },
+                    'Talk to the receptionist'
+                ]
+            },
+            {
+                type: 'list',
+                name: 'size',
+                message: 'What size do you need?',
+                choices: ['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro'],
+                filter: function (val) {
+                    return val.toLowerCase();
+                }
+            }
+        ])
+        .then(answers => {
+            console.log(JSON.stringify(answers, null, '  '));
+        });
 }
