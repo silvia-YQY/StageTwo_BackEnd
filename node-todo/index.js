d532b074a240a98e373d3e2ebe4fa7981a1c2171
silvia-YQY <545067444@qq.com>
2019-11-24 14:17:17 +0800

    style:函数式提取功能点

--- node-todo/index.js
+++ node-todo/index.js
@@ -14,8 +14,77 @@ module.exports.clear = async (title) => {
     await db.write([])
 }
 
-module.exports.showAll = async () => {
-    const list = await db.read()
+function markAsDone (list, index) {
+    list[index].done = true
+    db.write(list)
+}
+
+function markAsUndone (list, index) {
+    list[index].done = false
+    db.write(list)
+}
+
+function updateTitle (list, index) {
+    inquirer.prompt({
+        type: 'input',
+        name: 'title',
+        message: '新的标题为？',
+        default: list[index].title
+    }).then(answer => {
+        list[index].title = answer.title
+        db.write(list)
+    });
+}
+
+function remove (list, index) {
+    list.splice(index, 1)
+    db.write(list)
+}
+
+function askForCreatTask (list) {
+    inquirer.prompt({
+        type: 'input',
+        name: 'title',
+        message: '请输入任务标题',
+    }).then(answer => {
+        list.push({
+            title: answer.title,
+            done: false
+        })
+        db.write(list)
+    });
+}
+
+function askForAction (list, index) {
+    inquirer.prompt({
+        type: 'list', name: 'action',
+        message: '请选择操作',
+        choices: [
+            { name: '退出', value: 'quit' },
+            { name: '已完成', value: 'markAsDone' },
+            { name: '未完成', value: 'markAsUndone' },
+            { name: '改标题', value: 'updateTitle' },
+            { name: '删除', value: 'remove' }
+        ]
+    }).then(answers2 => {
+        switch (answers2.action) {
+            case 'markAsDone':
+                markAsDone(list, index)
+                break;
+            case 'markAsUndone':
+                markAsUndone(list, index)
+                break;
+            case 'updateTitle':
+                updateTitle(list, index)
+                break;
+            case 'remove':
+                remove(list, index)
+                break;
+        }
+    })
+}
+
+function printTasks (list) {
     inquirer
         .prompt([
             {
@@ -32,44 +101,18 @@ module.exports.showAll = async () => {
         .then(answers => {
             const index = parseInt(answers.index)
             if (index >= 0) {
-                inquirer.prompt({
-                    type: 'list', name: 'action',
-                    message: '请选择操作',
-                    choices: [
-                        { name: '退出', value: 'quit' },
-                        { name: '已完成', value: 'markAsDone' },
-                        { name: '未完成', value: 'markAsUndone' },
-                        { name: '改标题', value: 'updateTitle' },
-                        { name: '删除', value: 'remove' }
-                    ]
-                }).then(answers2 => {
-                    switch (answers2.action) {
-                        case 'markAsDone':
-                            list[index].done = true
-                            db.write(list)
-                            break;
-                        case 'markAsUndon':
-                            list[index].done = false
-                            db.write(list)
-                            break;
-                        case 'updateTitle':
-                            inquirer.prompt({
-                                type: 'input',
-                                name: 'title',
-                                message: '新的标题为？',
-                                default: list[index].title
-                            }).then(answer => {
-                                list[index].title = answer.title
-                                db.write(list)
-                            });
-                            break;
-                        case 'remove':
-                            list.splice(index, 1)
-                            break;
-                    }
-                })
-            } else if (index === '-2') {
-                console.log(answers);
+                // 询问任务
+                askForAction(list, index)
+            } else if (index === -2) {
+                // 创建任务
+                askForCreatTask(list)
             }
         });
+}
+
+module.exports.showAll = async () => {
+    // 获取之前的任务
+    const list = await db.read()
+    // 打印之前的任务
+    printTasks(list)
 }
