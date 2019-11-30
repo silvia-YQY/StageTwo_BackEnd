fd63a6c427c52a3bbb2946a105f830fb7f8c4e71
silvia-YQY <545067444@qq.com>
2019-11-30 20:00:26 +0800

    添加清理机制

--- node-todo/__mocks__/fs.js
+++ node-todo/__mocks__/fs.js
@@ -2,7 +2,7 @@ const fs = jest.genMockFromModule('fs')
 const _fs = jest.requireActual('fs')
 
 Object.assign(fs, _fs)
-const readMocks = {}
+let readMocks = {}
 
 fs.setReadFileMock = (path, err, data) => {
     readMocks[path] = [err, data]
@@ -21,7 +21,7 @@ fs.readFile = (path, options, callback) => {
     }
 }
 
-const writeMocks = {}
+let writeMocks = {}
 
 fs.setWriteFileMock = (path, fn) => {
     writeMocks[path] = fn
@@ -34,6 +34,11 @@ fs.writeFile = (path, data, options, callback) => {
     } else {
         _fs.writeFile(path, data, options, callback)
     }
+}
+
+fs.clearMocks = () => {
+    readMocks = {}
+    writeMocks = {}
 }
 
 module.exports = fs
