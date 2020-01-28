b425f4ef1480918c442f4937c46b3416e180691c
silvia-YQY <545067444@qq.com>
2020-01-28 13:32:54 +0800

    feat: 处理不存在的文件

--- node_server/index.ts
+++ node_server/index.ts
@@ -10,33 +10,30 @@ const publicDir = p.resolve(__dirname, 'public');
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
     const { method, url: path, headers } = request;
     console.log(path);
-    const { pathname, search } = url.parse(path);
-    switch (pathname) {
-        case '/index.html':
-            response.setHeader('Content-Type', 'text/html;charset=utf-8');
-            fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
-                if (error) throw error;
-                response.end(data.toString());
-            });
-            break;
-        case '/main.js':
-            response.setHeader('Content-Type', 'text/javascript;charset=utf-8');
-            fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
-                if (error) throw error;
-                response.end(data.toString());
-            });
-            break;
-        case '/style.css':
-            response.setHeader('Content-Type', 'text/css;charset=utf-8');
-            fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
-                if (error) throw error;
-                response.end(data.toString());
-            });
-            break;
-        default:
-            response.statusCode = 404;
-            response.end();
+    const { pathname: pathName, search } = url.parse(path);
+    let fileName = pathName.substr(1);
+    if (fileName === '') {
+        fileName = 'index.html';
     }
+    // response.setHeader('Content-Type', 'text/html;charset=utf-8');
+    fs.readFile(p.resolve(publicDir, fileName), (error, data) => {
+        if (error) {
+            if (error.errno === -4058) {
+                response.statusCode = 404;
+                fs.readFile(
+                    p.resolve(publicDir, '404/index.html'),
+                    (err, data) => {
+                        response.end(data);
+                    }
+                );
+            } else {
+                response.statusCode = 500;
+                response.end('服务器繁忙');
+            }
+        } else {
+            response.end(data);
+        }
+    });
 });
 
 server.listen(8888, () => {
