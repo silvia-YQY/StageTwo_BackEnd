90f94e85e16de7581f853e4c8d741beb9cc53e9b
silvia-YQY <545067444@qq.com>
2020-01-28 12:56:24 +0800

    feat:处理查询字符串

--- node_server/index.ts
+++ node_server/index.ts
@@ -2,13 +2,16 @@ import * as http from 'http';
 import { IncomingMessage, ServerResponse } from 'http';
 import * as fs from 'fs';
 import * as p from 'path';
+import * as url from 'url';
 
 const server = http.createServer();
 const publicDir = p.resolve(__dirname, 'public');
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-    const { method, url, headers } = request;
-    switch (url) {
+    const { method, url: path, headers } = request;
+    console.log(path);
+    const { pathname, search } = url.parse(path);
+    switch (pathname) {
         case '/index.html':
             response.setHeader('Content-Type', 'text/html;charset=utf-8');
             fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
@@ -30,6 +33,9 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
                 response.end(data.toString());
             });
             break;
+        default:
+            response.statusCode = 404;
+            response.end();
     }
 });
 
